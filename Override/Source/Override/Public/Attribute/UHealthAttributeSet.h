d5b9d46a9cca6822cc57583c148402798b449722
FERRARI <p.ferrari@rubika-edu.com>
2025-10-01 12:22:05 +0200

    Sete up GAS

--- Override/Source/Override/Public/Attribute/UHealthAttributeSet.h
+++ Override/Source/Override/Public/Attribute/UHealthAttributeSet.h
@@ -3,8 +3,11 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayEffectExtension.h"
 #include "UHealthAttributeSet.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FAttributeChangedEvent, UAttributeSet*, AttributeSet, float, OldValue,
+                                               float, NewValue);
 
 UCLASS()
 class OVERRIDE_API UHealthAttributeSet : public UAttributeSet
@@ -13,14 +16,34 @@ class OVERRIDE_API UHealthAttributeSet : public UAttributeSet
 
 public:
 	UHealthAttributeSet();
-	
+
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
- 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, ReplicatedUsing=OnRep_Health, meta = (HideFromModifiers))
 	FGameplayAttributeData Health;
-	 ATTRIBUTE_ACCESSORS_BASIC(UHealthAttributeSet, Health);
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	ATTRIBUTE_ACCESSORS_BASIC(UHealthAttributeSet, Health);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, ReplicatedUsing=OnRep_MaxHealth)
 	FGameplayAttributeData MaxHealth;
-	 ATTRIBUTE_ACCESSORS_BASIC(UHealthAttributeSet, MaxHealth);
+	ATTRIBUTE_ACCESSORS_BASIC(UHealthAttributeSet, MaxHealth);
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue);
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+
+	UPROPERTY(BlueprintAssignable)
+	FAttributeChangedEvent OnHealthChanged;
+
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+	
+	// Damage value calculated during a GE. Meta attribute.
+	UPROPERTY(VisibleAnywhere)
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS_BASIC(UHealthAttributeSet, Damage);
 };
