dd0681385804b5f3acf7d8a01a5c21f7d771de8a
Hazmat11 <92298810+Hazmat11@users.noreply.github.com>
2025-10-31 12:17:03 +0100

    New GameMode

---
+++ Override/Source/Override/Public/GameMode/GameModeStat.h
@@ -0,0 +1,35 @@
+// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameModeStat.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class OVERRIDE_API UGameModeStat : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GameMode")
+	TArray<int32> RoundData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GameMode")
+	int PhaseTime = 90;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GameMode")
+	float TransitionTime = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GameMode")
+	float RespawnTimeCooldown = 10.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GameMode")
+	int DataPourcentOnDeath = 20;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GameMode")
+	int DataPourcentOnTeamKill = 45;
+};
