e3633c0c2fd50775ca468c75b485012addcd6caf
FERRARI <p.ferrari@rubika-edu.com>
2025-12-15 12:06:34 +0100

    Adding httpserver module to extra moduls in targets.cs

--- Override/Source/OverrideServer.Target.cs
+++ Override/Source/OverrideServer.Target.cs
@@ -8,7 +8,7 @@ public class OverrideServerTarget : TargetRules
     {
         Type = TargetType.Server;
         DefaultBuildSettings = BuildSettingsVersion.V2;
+        ExtraModuleNames.AddRange(new string[] { "Override", "HttpServer" });
         CppStandard = CppStandardVersion.Latest;
-        ExtraModuleNames.Add("Override");
     }
 }
