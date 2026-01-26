771f61290b897faa061b6b12749af2d578b4992b
FERRARI <p.ferrari@rubika-edu.com>
2026-01-26 16:56:26 +0100

    GameMode cpp

---
+++ Override/Source/Override/Public/FirstPersonGameMode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstPersonGameMode.generated.h"
+
+UCLASS()
+class OVERRIDE_API AFirstPersonGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
