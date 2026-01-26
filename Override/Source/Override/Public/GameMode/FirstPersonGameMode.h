b44b02543c12757ae6b1b1b3dc470fe341bbca11
FERRARI <p.ferrari@rubika-edu.com>
2026-01-26 18:22:21 +0100

    Socket TCP gamemod game data server

--- Override/Source/Override/Public/GameMode/FirstPersonGameMode.h
+++ Override/Source/Override/Public/GameMode/FirstPersonGameMode.h
@@ -8,4 +8,11 @@ UCLASS()
 class OVERRIDE_API AFirstPersonGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	static void SendDataToDB();
+
+private:
+	static FString GetVersionFromFile(const FString& FilePath);
 };
