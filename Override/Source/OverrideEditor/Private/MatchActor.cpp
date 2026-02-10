bcb78e97c2b533b942b9e70c0d6dc73a1663c79c
FERRARI <p.ferrari@rubika-edu.com>
2026-02-10 10:51:00 +0100

    Fix IP

--- Override/Source/OverrideEditor/Private/MatchActor.cpp
+++ Override/Source/OverrideEditor/Private/MatchActor.cpp
@@ -1,4 +1,3 @@
-
 #include "OverrideEditor/Public/MatchActor.h"
 
 #include "GameMode/MatchPlayerData.h"
@@ -9,6 +8,10 @@ void AMatchActor::OnConstruction(const FTransform& Transform)
 	if (!GetWorld()) return;
 
 	FlushPersistentDebugLines(GetWorld());
+	
+	auto team1Count = 0;
+    auto team2Count = 0;
+    auto team3Count = 0;
 
 	for (const FMatchPlayerData& Player : Players)
 	{
@@ -17,34 +20,44 @@ void AMatchActor::OnConstruction(const FTransform& Transform)
 		switch (Player.TeamId)
 		{
 		case 0:
-			Color = FColor::Blue;
+			Color = team1Count == 0 ? FColor::Blue : FColor::Cyan;
+			team1Count++;
 			break;
 		case 1:
-			Color = FColor::Magenta;
+			Color = team2Count == 0 ? FColor::Magenta : FColor::Red;
+			team2Count++;
 			break;
 		case 2:
-			Color = FColor::Orange;
+			Color = team3Count == 0 ? FColor::Orange : FColor::Yellow;
+			team3Count++;
 			break;
 		default:
 			Color = FColor::Black;
 			break;
 		}
-		
+
 		int Depth = SeeThrough ? 100 : 0;
-		
+
 		for (int32 i = 1; i < Player.Positions.Num(); ++i)
 		{
-			DrawDebugLine(
-				GetWorld(),
-				Player.Positions[i - 1].Position,
-				Player.Positions[i].Position,
-				Color,
-				true,
-				-1.f,
-				Depth,
-				8
-			);
+			FVector prevPos = Player.Positions[i - 1].Position;
+			FVector pos = Player.Positions[i].Position;
 
+
+			if (FVector::Dist(prevPos, pos) < 100)
+			{
+				DrawDebugLine(
+					GetWorld(),
+					prevPos,
+					pos,
+					Color,
+					true,
+					-1.f,
+					Depth,
+					8
+				);
+			}
+			
 			DrawDebugPoint(
 				GetWorld(),
 				Player.Positions[i].Position,
