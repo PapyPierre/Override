28fa3d1a922bb96928bfb163c0662216630d29ac
FERRARI <p.ferrari@rubika-edu.com>
2026-03-09 15:02:29 +0100

    Version.h

--- Override/Source/Override/Private/BlueprintHelpers.cpp
+++ Override/Source/Override/Private/BlueprintHelpers.cpp
@@ -1,16 +1,8 @@
 #include "BlueprintHelpers.h"
+#include "Version.h"
 #include "Misc/ConfigCacheIni.h"
 
 FString UBlueprintHelpers::GetProjectVersion()
 {
-	FString Version;
-
-	GConfig->GetString(
-		TEXT("/Script/EngineSettings.GeneralProjectSettings"),
-		TEXT("ProjectVersion"),
-		Version,
-		GGameIni
-	);
-
-	return Version;
+	return TEXT(GAME_VERSION);
 }
